024194c66894898e13a5bf34c071d6de046de404
metidjisidahmed <is_metidji@esi.dz>
2023-01-07 10:10:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+const studentSchema = require('./models/student.model')
+
+
+
+mongoose.set('strictQuery', false);
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const url ='mongodb://127.0.0.1:27017/Gomycode' ;
+const connect = mongoose.connect(url );
+connect.then((db)=>{
+  console.log(url);
+  console.log('The DataBase is connected with the server nowww ')
+
+}).catch((err)=>{
+  console.log("Mongoose Connection Error =" , err.message)
+});
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/student.model.js
@@ -0,0 +1,45 @@
+let mongoose=require('mongoose')
+let Schema =mongoose.Schema
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+
+var studentSchema = new Schema({
+     fullName : {
+         firstName : {
+             type : Schema.Types.String,
+             required : true
+         },
+         lastName : {
+             type : Schema.Types.String,
+             required : true
+         }
+     },
+    // source :  https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+    email: {
+        type: Schema.Types.String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    groups : [{
+         type : Schema.Types.ObjectId,
+         ref : "Group"
+    }] ,
+    gender : {
+        enum: ['M', 'F']
+    },
+    birthday : {
+         type : Schema.Types.Date
+    }
+}, {
+    timestamps: true
+})
+
+module.exports= mongoose.model("Student"  , studentSchema )
